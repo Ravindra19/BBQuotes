5f1a36519d7f9a1598afda4e5306341c5a5a171c
Ravindra Gaikwad <ravindragaikwad@Ravindras-MacBook-Pro.local>
2025-10-08 11:44:25 +0530

    First Commit

---
+++ BBQuotes/BBQuotes/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  BBQuotes
+//
+//  Created by Ravindra Gaikwad on 25/09/25.
+//
+
+import Foundation
+
+enum Constants {
+    static let bbName = "Breaking Bad"
+    static let bcsName = "Better Call Saul"
+    static let ecName = "El Camino"
+}
---
+++ BBQuotes/BBQuotes/Death.swift
@@ -0,0 +1,15 @@
+//
+//  Death.swift
+//  BBQuotes
+//
+//  Created by Ravindra Gaikwad on 23/09/25.
+//
+
+import Foundation
+
+struct Death: Decodable {
+    let character: String
+    let image: URL
+    let details: String
+    let lastWords: String
+}
